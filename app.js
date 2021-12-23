8d1a527d6ee09dd56fe44fe7b4ffb54ca6e41993
gitName <a@b.c>
2021-12-23 17:01:27 -0300

    get using XMLHttpRequest not implemented

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-const http = require('http');
+const XMLHttpRequest = require('xhr2');
+const Http = new XMLHttpRequest();
+
 const express = require('express');
 const app = express();
+const router = express.Router();
 
-const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
 const port = 3000;
 
 
+
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
+  }
+
 async function connect2url(){
-    
 
     try{
         var server = app.listen(3000);
         console.log('listening on port 3000');
+        console.log(url);
+
     } catch (err){
         console.error(err);
+
     } finally {
         server.close();
         console.log('closing connection on port 3000');
     }
 
 }
-
-connect2url();
