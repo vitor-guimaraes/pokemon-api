96d2690f026e7be6294c4eded607ffae01e2bac7
gitName <a@b.c>
2021-12-28 10:48:46 -0300

    list pokemons

--- app.js
+++ app.js
@@ -4,43 +4,63 @@ const Http = new XMLHttpRequest();
 const mongoose = require('mongoose');
 
 
+
+
 const express = require('express');
+const { Http2ServerResponse } = require('http2');
+const { object } = require('webidl-conversions');
 const app = express();
 const router = express.Router();
 
 
 const port = 3000;
 
-
+//CONNECT TO MONGODB
+const dbUser = 'admin';
+const dbPassword = 'password1234';
+const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8bj40.mongodb.net/pokedex?retryWrites=true&w=majority`;
+const {MongoClient} = require('mongodb');
+const client = new MongoClient(dbURI);  
 
 
 connect2url();
-getPokemon('ditto');
+// getPokemon('ditto');
+// getPokemonName('ditto');
+// getPokemonName('?limit=151');
+
+// listPokemons(5);
+
+// createListing();
+
 
 async function connect2url(){
 
     try{
         // var server = app.listen(3000);
         // console.log('listening on port 3000');
-        // console.log(url);
-
-        const dbUser = 'admin';
-        const dbPassword = 'password1234';
-        const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8bj40.mongodb.net/pokedex?retryWrites=true&w=majority`;
+        // console.log(url);        
         
-        mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(console.log('connected to pokedex'))
-        .catch((err) => console.log(err));
+        // mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        // .then(console.log('connected to pokedex'))
+        // .catch((err) => console.log(err));
+
+        await client.connect()
+        .then (result = console.log('connected to pokedex\n')) 
+        .then (listPokemons(5));
+
+
 
     } catch (err){
         console.error(err);
 
     } finally {
         // server.close();
-        // console.log('closing connection on port 3000');
-        await getPokemon()
-        .then(mongoose.connection.close());
-        console.log('closing connection on pokedex');
+        // console.log('closing connection on port 3000');        
+        // .then(mongoose.connection.close());
+
+        console.log('\nclosing connection on pokedex\n');
+        client.close();
+        
     }
 
 }
@@ -61,3 +81,21 @@ async function getPokemon(pokemon){
         console.error(err);
     }
 };
+
+async function listPokemons(number){
+    const url = `https://pokeapi.co/api/v2/pokemon/?limit=${number}`;
+
+    console.log(`First ${number} Pokemons are: `);
+
+    Http.open('get', url);
+    Http.send();
+    Http.onreadystatechange = (e) => {
+        const resp = Http.responseText;
+        console.log(resp);
+    };    
+}
+
+async function createListing(connect2url, newListing){
+    const result = await connect2url.db("pokedex").collection("pokemons").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
